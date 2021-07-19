2291ae49a6b74fb6a110d9de02743accaf7ed348
Blaire <grant.blaire@gmail.com>
2021-07-18 20:54:49 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,32 @@
 // your Bomb code here!
+// your ImageSlider code here!
+// your ImageSlider code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    // increment = () => {
+    //     const newCount = this.state.count + 1
+    //     this.setState({
+    //       count: newCount
+    //     })
+    // }    
+    secondsLeftFunction = () => {
+        return this.state.secondsLeft === 0 ? "Boom!" : `${this.state.secondsLeft} seconds left before I go boom!`
+    }
+
+    render() {
+        return (
+        <div id="bomb">
+            <h1>{this.secondsLeftFunction()}</h1>
+        </div>
+        )
+    }
+}
